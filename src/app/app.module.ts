310dccb919eae36d4f9c429a2bda2ba0e427fc17
Merna Hammam <miernahammam5@gmail.com>
2021-01-10 07:16:47 +0200

    Creating Components and Importing Scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductItemComponent } from './components/products-list/product-item/product-item.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CartItemComponent } from './components/cart/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProductsListComponent,
+    ProductItemComponent,
+    CartComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule,
